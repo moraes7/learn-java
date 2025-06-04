24a7964c7f448d7f2d7c11942d510af64f931468
moraes7 <nicolasmoraes.dev@gmail.com>
2025-06-04 11:15:52 -0300

    classes abstratas aula 1

---
+++ src/com/nicolasmoraes/learnjava/javacore/Lclassesabstratas/dominio/Funcionario.java
@@ -0,0 +1,11 @@
+package com.nicolasmoraes.learnjava.javacore.Lclassesabstratas.dominio;
+
+public abstract class Funcionario {
+    protected String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+}
---
+++ src/com/nicolasmoraes/learnjava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,13 @@
+package com.nicolasmoraes.learnjava.javacore.Lclassesabstratas.test;
+
+import com.nicolasmoraes.learnjava.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import com.nicolasmoraes.learnjava.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Nami", 5000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Nicolas", 3000);
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
+}
