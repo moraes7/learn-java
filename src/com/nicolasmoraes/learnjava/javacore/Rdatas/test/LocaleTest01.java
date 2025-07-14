82e39ceca0f405a94575db4219f49ba71bd29140
moraes7 <nicolasmoraes.dev@gmail.com>
2025-07-14 11:33:02 -0300

    classes utilitarias - locale

---
+++ src/com/nicolasmoraes/learnjava/javacore/Rdatas/test/LocaleTest01.java
@@ -0,0 +1,43 @@
+package com.nicolasmoraes.learnjava.javacore.Rdatas.test;
+
+// Classes Utilitárias - Internacionalização Datas com Locale
+
+import com.nicolasmoraes.learnjava.javacore.Gassociacao.ExercicioAssociacao.dominio.Local;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        // Italiano - Itália
+        Locale localeItaly = new Locale("it", "IT");
+
+        // Italiano - Suiça
+        Locale localeCH = new Locale("it", "CH");
+
+        // Hindi - Índia
+        Locale localeIndia = new Locale("hi", "IN");
+
+        // Japones - Japão
+        Locale localeJapan = new Locale("ja", "JP");
+
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeCH);
+        DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL, localeIndia);
+        DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL, localeJapan);
+
+        System.out.println("Data formatada Itália: "+df1.format(calendar.getTime())); // lunedì 14 luglio 2025
+        System.out.println("Data formatada Suiça: "+df2.format(calendar.getTime())); // lunedì, 14 luglio 2025
+        System.out.println("Data formatada Índia: "+df3.format(calendar.getTime())); // सोमवार, 14 जुलाई 2025
+        System.out.println("Data formatada Japão: "+df4.format(calendar.getTime())); // 2025年7月14日月曜日
+
+        // exibindo idioma do sistema operacional
+        System.out.println(localeItaly.getDisplayCountry(localeJapan)); // イタリア
+        System.out.println(localeCH.getDisplayCountry(localeItaly)); // Svizzera
+
+        // como é falado italiano em japones
+        System.out.println(localeItaly.getDisplayLanguage(localeJapan)); // イタリア語
+    }
+}
