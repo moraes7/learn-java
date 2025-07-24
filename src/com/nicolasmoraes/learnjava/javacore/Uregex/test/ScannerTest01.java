9b74cf838b762c247862654aef3bfb3f61b3f4a0
moraes7 <nicolasmoraes.dev@gmail.com>
2025-07-24 10:52:34 -0300

    sncanner-tokens e delimitadores

---
+++ src/com/nicolasmoraes/learnjava/javacore/Uregex/test/ScannerTest01.java
@@ -0,0 +1,20 @@
+package com.nicolasmoraes.learnjava.javacore.Uregex.test;
+
+//  Classes Utilitárias - Scanner - Tokens e Delimitadores
+
+public class ScannerTest01 {
+    public static void main(String[] args) {
+        String texto = "Levi, Eren, Mikasa, true, 200";
+        String[] nomes = texto.split(",");
+        for (String nome : nomes) {
+            System.out.println(nome.trim());
+            /*
+            Levi
+            Eren
+            Mikasa
+            true
+            200
+             */
+        }
+    }
+}
