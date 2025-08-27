1c6daeb8d2e6d47b0553ae64d67ea88a1c7dfd8d
moraes7 <nicolasmoraes.dev@gmail.com>
2025-08-27 11:56:30 -0300

    feat: coleçoes - equals pt 01

---
+++ src/com/nicolasmoraes/learnjava/javacore/Ycolecoes/equals/test/EqualsTest01.java
@@ -0,0 +1,23 @@
+package com.nicolasmoraes.learnjava.javacore.Ycolecoes.equals.test;
+
+import com.nicolasmoraes.learnjava.javacore.Ycolecoes.equals.dominio.Smartphone;
+
+public class EqualsTest01 {
+    public static void main(String[] args) {
+        // Coleções pt 01 - equals pt 01
+
+        String nome1 = "Nicolas Moraes";
+        String nome2 = "Nicolas Moraes";
+        System.out.println(nome1.equals(nome2)); // true
+
+        // =====================================================================================
+
+        Smartphone s1 = new Smartphone("1ABC1", "iPhone");
+        Smartphone s2 = new Smartphone("1ABC1", "iPhone");
+        System.out.println(s1.equals(s2)); // false
+        s2 = s1;
+        System.out.println(s1.equals(s2)); // true
+
+
+    }
+}
