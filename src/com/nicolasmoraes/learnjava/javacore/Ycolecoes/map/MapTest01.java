72b6b91674c982cdb09a9ffef9946553ad97f083
moraes7 <nicolasmoraes.dev@gmail.com>
2025-09-10 10:31:00 -0300

    feat: coleçoes - Map, HashMap, LinkedHashMap pt 01

---
+++ src/com/nicolasmoraes/learnjava/javacore/Ycolecoes/map/MapTest01.java
@@ -0,0 +1,34 @@
+package com.nicolasmoraes.learnjava.javacore.Ycolecoes.map;
+
+// Coleções pt 18 - Map, HashMap, LinkedHashMap pt 01
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest01 {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("teklado", "teclado");
+        map.put("mouze", "mouse");
+        map.put("vc", "você");
+        map.putIfAbsent("vc2", "você");
+
+        for (String key : map.keySet()) {
+            System.out.println(key + " = " + map.get(key));
+            /* vc2 = você
+               teklado = teclado
+               mouze = mouse
+               vc = você */
+        }
+
+        System.out.println("---------------------------");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+            /* vc2 = você
+               teklado = teclado
+               mouze = mouse
+               vc = você */
+        }
+    }
+}
