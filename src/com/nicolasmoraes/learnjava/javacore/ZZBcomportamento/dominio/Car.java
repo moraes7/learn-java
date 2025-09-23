59d474dfbf2e04af47b7e5940851ad023d20727a
moraes7 <nicolasmoraes.dev@gmail.com>
2025-09-23 14:33:15 -0300

    feat: parametrizando comportamentos pt 01

---
+++ src/com/nicolasmoraes/learnjava/javacore/ZZBcomportamento/dominio/Car.java
@@ -0,0 +1,33 @@
+package com.nicolasmoraes.learnjava.javacore.ZZBcomportamento.dominio;
+
+public class Car {
+    private String brand = "Audi";
+    private String color;
+    private int year;
+
+    public Car(String color, int year) {
+        this.color = color;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
