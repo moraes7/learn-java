83c9a4f44671757dfe37159f84ccf5805afb2aad
moraes7 <nicolasmoraes.dev@gmail.com>
2025-10-16 16:44:39 -0300

    feat: streams pt 08 - reduce pt 02

---
+++ src/com/nicolasmoraes/learnjava/javacore/ZZEstreams/test/StreamTest08.java
@@ -0,0 +1,36 @@
+package com.nicolasmoraes.learnjava.javacore.ZZEstreams.test;
+
+// Streams pt 08 - Reduce pt 02
+
+import com.nicolasmoraes.learnjava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamTest08 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 10.99),
+            new LightNovel("Violet Envergarden", 5.99),
+            new LightNovel("No Game no Life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo Desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+        // somando todos os preços acima de 3
+        lightNovels.stream()
+                .map(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .reduce(Double::sum)
+                .ifPresent(System.out::println); // 35.96
+
+        // streams de tipos primitivos
+        double sum = lightNovels.stream()
+                .mapToDouble(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .sum();
+        System.out.println(sum); // 35.96
+    }
+}
