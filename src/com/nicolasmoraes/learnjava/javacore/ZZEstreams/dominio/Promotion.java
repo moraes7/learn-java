097db80753f7c20a93eb6532c07c7403d0e959c2
moraes7 <nicolasmoraes.dev@gmail.com>
2025-10-27 10:53:47 -0300

    feat: streams pt 12 - collectors pt 03 - grouping by pt 02

---
+++ src/com/nicolasmoraes/learnjava/javacore/ZZEstreams/dominio/Promotion.java
@@ -0,0 +1,5 @@
+package com.nicolasmoraes.learnjava.javacore.ZZEstreams.dominio;
+
+public enum Promotion {
+    NORMAL_PRICE, UNDER_PROMOTION
+}
---
+++ src/com/nicolasmoraes/learnjava/javacore/ZZEstreams/test/StreamTest13.java
@@ -0,0 +1,36 @@
+package com.nicolasmoraes.learnjava.javacore.ZZEstreams.test;
+
+// Streams pt 12 - Collectors pt 03 - Grouping by pt 02
+
+import com.nicolasmoraes.learnjava.javacore.ZZEstreams.dominio.Category;
+import com.nicolasmoraes.learnjava.javacore.ZZEstreams.dominio.LightNovel;
+import com.nicolasmoraes.learnjava.javacore.ZZEstreams.dominio.Promotion;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamTest13 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99, Category.FANTASY),
+            new LightNovel("Overlord", 10.99, Category.FANTASY),
+            new LightNovel("Violet Envergarden", 5.99, Category.DRAMA),
+            new LightNovel("No Game no Life", 2.99, Category.FANTASY),
+            new LightNovel("Fullmetal Alchemist", 5.99, Category.FANTASY),
+            new LightNovel("Kumo Desuga", 1.99, Category.FANTASY),
+            new LightNovel("Monogatari", 4.00, Category.ROMANCE)
+    ));
+
+    public static void main(String[] args) {
+        // Coletando Light Novel em promoção e com preço normal
+        Map<Promotion, List<LightNovel>> collect = lightNovels.stream().collect(Collectors.groupingBy(ln -> ln.getPrice() < 6 ? Promotion.UNDER_PROMOTION : Promotion.NORMAL_PRICE));
+        System.out.println(collect);
+
+        // Coletando Light Novel em promoção e preços normais dentro de cada categoria
+        Map<Category, Map<Promotion, List<LightNovel>>> collect1 = lightNovels.stream().collect(Collectors.groupingBy(LightNovel::getCategory, Collectors.groupingBy(ln -> ln.getPrice() < 6 ? Promotion.UNDER_PROMOTION : Promotion.NORMAL_PRICE)));
+        System.out.println(collect1);
+
+
+    }
+}
