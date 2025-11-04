dcca216052816cf09d9c6251d03e754775e4c3d8
moraes7 <nicolasmoraes.dev@gmail.com>
2025-11-04 16:45:37 -0300

    feat: jdbc, configurando maven

---
+++ src/main/java/com/nicolasmoraes/learnjava/javacore/ZZIjdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package com.nicolasmoraes.learnjava.javacore.ZZIjdbc.test;
+
+import com.nicolasmoraes.learnjava.javacore.ZZIjdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
