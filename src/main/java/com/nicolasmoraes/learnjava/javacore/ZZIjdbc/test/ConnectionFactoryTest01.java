ba453c34c6baebd6d679afd0df8215ff72bd52f7
moraes7 <nicolasmoraes.dev@gmail.com>
2025-11-06 16:49:36 -0300

    feat: JDBC pt 09 - atualizando dados com Statement

--- src/main/java/com/nicolasmoraes/learnjava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/com/nicolasmoraes/learnjava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -8,12 +8,10 @@ import lombok.extern.log4j.Log4j2;
 public class ConnectionFactoryTest01  {
     public static void main(String[] args) {
         Producer producer = Producer.builder().name("Studio Deen").build();
+        Producer producerToUpdate = Producer.builder().id(1).name("MADHOUSE").build();
+
 //        ProducerService.save(producer);
-        ProducerService.delete(18);
-        ProducerService.delete(19);
-        ProducerService.delete(20);
-        ProducerService.delete(21);
-        ProducerService.delete(22);
-        ProducerService.delete(23);
+//        ProducerService.delete(19);
+        ProducerService.update(producerToUpdate);
     }
 }
