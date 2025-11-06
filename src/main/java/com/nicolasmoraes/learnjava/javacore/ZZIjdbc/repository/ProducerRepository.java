38ea67f1ae41c6e19bcf84c1816ca3cab189728d
moraes7 <nicolasmoraes.dev@gmail.com>
2025-11-06 12:36:37 -0300

    feat: JDBC pt 07 - Lombok e Log4j2

--- src/main/java/com/nicolasmoraes/learnjava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/main/java/com/nicolasmoraes/learnjava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -2,18 +2,20 @@ package com.nicolasmoraes.learnjava.javacore.ZZIjdbc.repository;
 
 import com.nicolasmoraes.learnjava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import com.nicolasmoraes.learnjava.javacore.ZZIjdbc.domain.Producer;
+import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+@Log4j2
 public class ProducerRepository {
     public static void save(Producer producer)  {
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');".formatted(producer.getName());
         try(Connection conn = ConnectionFactory.getConnection();
             Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
-            System.out.println(rowsAffected);
+            log.info("Inserted producer in the database, rows affected '{}'", rowsAffected);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/com/nicolasmoraes/learnjava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/com/nicolasmoraes/learnjava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -3,10 +3,16 @@ package com.nicolasmoraes.learnjava.javacore.ZZIjdbc.test;
 import com.nicolasmoraes.learnjava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import com.nicolasmoraes.learnjava.javacore.ZZIjdbc.domain.Producer;
 import com.nicolasmoraes.learnjava.javacore.ZZIjdbc.repository.ProducerRepository;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class ConnectionFactoryTest01  {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("NHK").build();
+        Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
+//        log.info("INFO");
+//        log.debug("DEBUG");
+//        log.warn("WARN");
+//        log.trace("TRACE");
     }
 }
