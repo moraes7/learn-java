a5c82c77da5bc808f36e5d4abc84796b71872e26
moraes7 <nicolasmoraes.dev@gmail.com>
2025-11-17 15:07:39 -0300

    feat: Padroes de Projeto pt 04 - Singleton pt 02 - Lazy Initialization

---
+++ src/main/java/com/nicolasmoraes/learnjava/javacore/ZZHpadroesdeprojeto/dominio/AircraftSingletonLazy.java
@@ -0,0 +1,36 @@
+package com.nicolasmoraes.learnjava.javacore.ZZHpadroesdeprojeto.dominio;
+
+// Padrões de Projeto pt 04 - Singleton pt 02 - Lazy Initialization
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AircraftSingletonLazy {
+    private static AircraftSingletonLazy INSTANCE;
+    private final Set<String> availableSeats = new HashSet<>();
+    private final String name;
+
+    private AircraftSingletonLazy(String name) {
+        this.name = name;
+    }
+
+    {
+        availableSeats.add("1A");
+        availableSeats.add("1B");
+    }
+
+    public static AircraftSingletonLazy getINSTANCE() {
+        if (INSTANCE == null) {
+            synchronized (AircraftSingletonLazy.class) {
+                if (INSTANCE == null) {
+                INSTANCE = new AircraftSingletonLazy("787-900");
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public boolean bookSeat(String seat) {
+        return availableSeats.remove(seat);
+    }
+}
