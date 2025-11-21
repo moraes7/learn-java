10f9b3e2a12e939eb92900775d334264890590a6
moraes7 <nicolasmoraes.dev@gmail.com>
2025-11-21 15:56:20 -0300

    feat: Testes unitários com jUnit pt 02

--- src/main/java/com/nicolasmoraes/learnjava/javacore/ZZKjunit/service/PersonService.java
+++ src/main/java/com/nicolasmoraes/learnjava/javacore/ZZKjunit/service/PersonService.java
@@ -2,11 +2,20 @@ package com.nicolasmoraes.learnjava.javacore.ZZKjunit.service;
 
 import com.nicolasmoraes.learnjava.javacore.ZZKjunit.dominio.Person;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class PersonService {
     public boolean isAdult(Person person) {
-        Objects.requireNonNull(person, "Person can't be null");
+//        Objects.requireNonNull(person, "Person can't be null");
+        if (person == null) {
+            throw new IllegalArgumentException();
+        }
         return person.getAge() >= 18;
+    }
+
+    public List<Person> filterRemovingNotAdult(List<Person> personList) {
+        return personList.stream().filter(this::isAdult).collect(Collectors.toList());
     }
 }
